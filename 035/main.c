46f703a6eef533b45e6051e167d59703ba2af75a
Austen Dicken <cvpcsm@gmail.com>
2011-08-30 13:55:06 -0500

    prob 35

---
+++ 035/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gmp.h>
+
+void rotate_str(char* s) {
+	char c = s[0];
+	int l = strlen(s);
+	int i;
+
+	for (i = 1; i < l; i++) {
+		s[i - 1] = s[i];
+	}
+
+	s[l - 1] = c;
+}
+
+int is_circular(mpz_t n) {
+	char* s = mpz_get_str(NULL, 10, n);
+	int l = strlen(s);
+	mpz_t m;
+	int i;
+	int r;
+
+	mpz_init(m);
+	r = 1;
+
+	for (i = 0; i < l; i++) {
+		mpz_set_str(m, s, 10);
+		if (mpz_probab_prime_p(m, 10) == 0) {
+			r = 0;
+			break;
+		}
+		rotate_str(s);
+	}
+
+	mpz_clear(m);
+	free(s);
+
+	return r;
+}
+
+int main(int argc, char* argv[]) {
+	mpz_t n;
+	int i;
+
+	mpz_init(n);
+	mpz_set_ui(n, 1);
+	mpz_nextprime(n, n);
+
+	i = 0;
+
+	while(mpz_get_ui(n) < 1000000) {
+		if (is_circular(n)) {
+			i++;
+		}
+		mpz_nextprime(n, n);
+	}
+
+	printf("%d\n", i);
+
+	mpz_clear(n);
+
+	return 0;
+}
